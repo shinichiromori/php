df6347a62613be18d027c2279e1e48f7f65b10a8
mori <forestcrossday@gmail.com>
2019-10-03 14:58:41 +0000

    PHP/Laravel 03 課題

--- php03.php
+++ php03.php
@@ -1,2 +1,38 @@
-森晋一朗です。
-１９８６年８月１６日
+<?php
+ 
+ $total = 0;
+ echo $total;
+ for ($i = 0; $i <= 100; $i++) {
+  $total += $i;
+ }
+ echo $total;
+ 
+ //課題１
+ 
+ $name = 'shinichiro';
+
+if ($name =='shinichiro') {
+ echo "私はしんいちろうです。";
+} else {
+ echo "あなたの名前ではありません";
+}
+
+//課題２
+$total = 0;
+echo $total;
+for ($i = 0; $i <= 10000; $i++) {
+  $total += $i;
+}
+echo $total;
+
+//課題３
+$fruits = array("Apple","Apricot","Banana","Grape",);
+
+ echo count($fruits);
+ for ($i = 0; $i < count($fruits); $i++) {
+  echo $fruits[$i];
+  echo "\n";
+}
+
+
+?>
