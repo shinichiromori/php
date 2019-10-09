e4fda410dd1cc8a9da9b33b3451b5f8b407fc863
mori <forestcrossday@gmail.com>
2019-10-09 12:25:59 +0000

    再度提出

--- php03.php
+++ php03.php
@@ -34,5 +34,25 @@ $fruits = array("Apple","Apricot","Banana","Grape",);
   echo "\n";
 }
 
+//課題３ 再提出
+  $fruits = array("Apple","Apricot","Banana","Grape",);
+  foreach($fruits as $fruit){
+  echo $fruit;
+  echo "\n";
+  }
+  
+//課題４  
+  
+ /* for文の始めの値を定義する /
+$start = 1;
+/* for文の終わりの値を定義する */
+$end = 101;
 
+ for($i = 1; $i < $end; $i++){
+  // 5で割り切れたら{}内を実行する
+  if($i % 5 == 0){
+    echo $i;
+    echo "\n";
+  }
+}
 ?>
