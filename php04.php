0bca53de03841dc77b492b6b5f302a074889b7ad
mori <forestcrossday@gmail.com>
2019-10-10 14:31:27 +0000

    課題４

--- php04.php
+++ php04.php
@@ -1,42 +1,44 @@
 <?php
 //課題1
-function outputDouble($num){
-$result = $num * 2;
-echo $result;
+function sum($max){
+ $result =$max*2;
+  return $result;
 }
- outputDouble(5);
+ echo sum(2);
  echo "\n";
-
+ ?>
+<?php 
 //課題2
 function f($a,$b){
  $result = $a + $b;
- echo $result;
+ return $result;
 }
-f(1,2);
+echo f(1,2);
 echo "\n";
-
-//課題３
+?>
+<?php
+//課題3
 $arr=array(1,3,5,7,9);
-echo array_product($arr)."\n";
-
-
-
+$total=1;
+foreach ($arr as $ar) {
+	$total *= $ar;
+}
+echo $total;
+echo "\n";
+?>
+<?php
 //課題４
-function max_array($arr){
+ function max_array($arr){
 // とりあえず配列の最初の要素を一番大きい値とする
  $max_number = $arr[0];
  foreach($arr as $a){
- //どうしたらいいかわからない・・・・
+ if($max_number <$a){
+    $max_number = $a;
  }
-
+ }
  return $max_number;
  }
-
-
-
-
-
-
-
-
+ echo max_array(array(10,20,30,40,50));
+ echo "\n";
  ?>
+ 
